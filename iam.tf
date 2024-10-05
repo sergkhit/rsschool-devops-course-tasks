4b359618c3e86af84b064ad8fdf79493ec3c16f6
sergkhit <135976790+sergkhit@users.noreply.github.com>
2024-10-05 18:47:26 +0300

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "role_for_github" {
+resource "aws_iam_role" "GithubActionsRole" {
   assume_role_policy = data.aws_iam_policy_document.github_actions_role_policy.json
   name               = "GithubActionsRole"
 }
